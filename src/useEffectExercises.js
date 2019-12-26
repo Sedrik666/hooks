aec019ed60b297a318eb52738b8aa18becd502a0
Sedrik666 <fedvetal@mail.ru>
2019-12-27 00:27:28 +0200

    study(useEffect): getData

---
+++ src/useEffectExercises.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
+    return(
+        <div>
+            {
+                visible&&(
+                    <div>
+                        <button onClick={() => setValue(value + 1)}>
+                            +
+                        </button>
+                        <HookCounter value={value}/>
+                        <Notification/>
+                    </div>
+                )
+            }
+            <button onClick={() => setVisible((visible) => !visible)}>
+                Toggle
+            </button>
+        </div>
+    )
+};
+
+const HookCounter = ({ value }) => {
+    useEffect(() => {
+        console.log('Component did mount');
+        return () => console.log('Component did unmount');
+    }, []);
+    useEffect(() => console.log('Component did update'));
+    useEffect(() => () => console.log('Component did unmount'), []);
+    return <p> {value} </p>
+};
+
+const Notification = () => {
+    const [visible, setVisible] = useState(true);
+    useEffect(() => {
+        const timeout  = setTimeout(
+            () => setVisible(false),
+            2500);
+        return () => clearTimeout(timeout)
+    }, []);
+    return(
+        visible&&(
+            <div><p>Hello</p></div>
+        )
+    );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
